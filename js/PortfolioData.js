4d89404a3491de90503132a5741b931a600364b4
Joensg <joen.sg@gmail.com>
2016-04-22 14:22:05 +0100

    added rps

--- js/PortfolioData.js
+++ js/PortfolioData.js
@@ -1,183 +1,191 @@
 //This file contains the portfolio_data
 
 var portfolio_data = {
-    portfolioitem : [
-        {
-            title : "Social Website Aggregator",
-            date : "March 2016",
-            href : "portfolioModal1",
-            src : "img/portfolio/siteace.png",
-            alt : "siteace.png",
-            link : "http://joensiteace.meteor.com/",
-            githublink : "https://github.com/Joensg/joensiteace.meteor.com/",
-            description : "A social website aggregator using MeteorJS."
-        },
-        {
-            title : "E-portfolio",
-            date : "November 2015",
-            href : "portfolioModal2",
-            src : "img/portfolio/eportfolio.png",
-            alt : "eportfolio.png",
-            link : "http://joensg.github.io/",
-            githublink : "https://github.com/Joensg/Joensg.github.io",
-            description : "My User Github Page Site. A site about me."
-        },
-        {
-            title : "Angular App1",
-            date : "February 2016",
-            href : "portfolioModal3",
-            src : "img/portfolio/angularapp1.png",
-            alt : "angularapp1.png",
-            link : "http://joensg.github.io/Web-Project-6-Angular-App1/",
-            githublink : "https://github.com/Joensg/Web-Project-6-Angular-App1/",
-            description : "Using the Angular Javascript framework, coded a one page application that downloads feed from a server and displays it in a table format."
-        },
-        {
-            title : "ConFusion",
-            date : "November 2015",
-            href : "portfolioModal4",
-            src : "img/portfolio/confusion.png",
-            alt : "confusion.png",
-            link : "http://joensg.github.io/Web-Project-5-ConFusion/",
-            githublink : "https://github.com/Joensg/Web-Project-5-ConFusion",
-            description : "An interactive website for a restaurant using bootstrap UI framework."
-        },
-        {
-            title : "Responsive Web page",
-            date : "November 2015",
-            href : "portfolioModal5",
-            src : "img/portfolio/responsivewebpage.png",
-            alt : "responsivewebpage.png",
-            link : "http://joensg.github.io/Web-Project-4-Responsive-web-page/",
-            githublink : "https://github.com/Joensg/Web-Project-4-Responsive-web-page",
-            description : "A responsive website about myself."
-        },
-        {
-            title : "Matching game",
-            date : "November 2015",
-            href : "portfolioModal6",
-            src : "img/portfolio/matchinggame.png",
-            alt : "matchinggame.png",
-            link : "http://joensg.github.io/Web-Project-3-Matching-game/",
-            githublink : "https://github.com/Joensg/Web-Project-3-Matching-game",
-            description : "A game to find and click on the extra smiley face present in the picture on the left side when compared to the picture on the right side."
-        },
-        {
-            title : "Simple Form",
-            date : "November 2015",
-            href : "portfolioModal7",
-            src : "img/portfolio/simpleform.png",
-            alt : "simpleform.png",
-            link : "http://joensg.github.io/Web-Project-1-Simple-form/",
-            githublink : "https://github.com/Joensg/Web-Project-1-Simple-form",
-            description : "A simple form for a website."
-        },
-        {
-            title : "Task List",
-            date : "February 2016",
-            href : "portfolioModal8",
-            src : "img/portfolio/tasklist.png",
-            alt : "tasklist.png",
-            link : "http://joensg.github.io/Web-Project-7-Task-List/index.html",
-            githublink : "https://github.com/Joensg/Web-Project-7-Task-List",
-            description : "A responsive website that presents a set of tasks to its users. The website is implemented using static content for the tasks, i.e. it is not data driven."
-        },
-        {
-            title : "Animals Data Driven Website",
-            date : "February 2016",
-            href : "portfolioModal9",
-            src : "img/portfolio/animalsddw.png",
-            alt : "animalsddw.png",
-            link : "http://joensg.github.io/Web-Project-8-Animals-Data-Driven-Website/",
-            githublink : "https://github.com/Joensg/Web-Project-8-Animals-Data-Driven-Website",
-            description : "A data driven website about animals using handlebarjs templates."
-        }
-    ],
-    certificateitem : [
-        {
-            number : "0",
-            src : "certificates/Coursera%20FXEYYPKMFY5N.png",
-            alt : "Coursera%20FXEYYPKMFY5N.png"
-        },
-        {
-            number : "1",
-            src : "certificates/Coursera%209KP9AA2Q4ABA.png",
-            alt : "Coursera%209KP9AA2Q4ABA.png"
-        },
-        {
-            number : "2",
-            src : "certificates/Coursera%20YG3N74UW4SVD.png",
-            alt : "Coursera%20YG3N74UW4SVD.png"
-        },
-        {
-            number : "3",
-            src : "certificates/Coursera%20LFATSDSLZHEP.png",
-            alt : "Coursera%20LFATSDSLZHEP.png"
-        },
-        {
-            number : "4",
-            src : "certificates/Coursera%20SVWBGWGE7MBD.png",
-            alt : "Coursera%20SVWBGWGE7MBD.png"
-        },
-        {
-            number : "5",
-            src : "certificates/Coursera%20N228ZZPXP57H.png",
-            alt : "Coursera%20N228ZZPXP57H.png"
-        },
-        {
-            number : "6",
-            src : "certificates/Coursera%20androidpart1.png",
-            alt : "Coursera%20androidpart1.png"
-        },
-        {
-            number : "7",
-            src : "certificates/Coursera%20androidpart2.png",
-            alt : "Coursera%20androidpart2.png"
-        },
-        {
-            number : "8",
-            src : "certificates/Coursera%20datascitoolbox.png",
-            alt : "Coursera%20datascitoolbox.png"
-        },
-        {
-            number : "9",
-            src : "certificates/Coursera%20GZCQ5F4MMQWB.png",
-            alt : "Coursera%20GZCQ5F4MMQWB.png"
-        },
-        {
-            number : "10",
-            src : "certificates/Coursera%20interactivepython1.png",
-            alt : "Coursera%20interactivepython1.png"
-        },
-        {
-            number : "11",
-            src : "certificates/Coursera%20interactivepython2.png",
-            alt : "Coursera%20interactivepython2.png"
-        },
-        {
-            number : "12",
-            src : "certificates/Coursera%20J2GMJGB2FABA.png",
-            alt : "Coursera%20J2GMJGB2FABA.png"
-        },
-        {
-            number : "13",
-            src : "certificates/Coursera%20py1%209DZSHBNBJGCQ.png",
-            alt : "Coursera%20py1%209DZSHBNBJGCQ.png"
-        },
-        {
-            number : "14",
-            src : "certificates/Coursera%20py2%207KVEDGH5TWYU.png",
-            alt : "Coursera%20py2%207KVEDGH5TWYU.png"
-        },
-        {
-            number : "15",
-            src : "certificates/Coursera%20YBGFQW5NTEXN.png",
-            alt : "Coursera%20YBGFQW5NTEXN.png"
-        },
-        {
-            number : "16",
-            src : "certificates/Coursera%20CGRE3UMZTU2U.png",
-            alt : "Coursera%20CGRE3UMZTU2U.png"
-        }
-    ]
+    portfolioitem: [
+    {
+        title: "Rock-Paper-Scissors",
+        date: "April 2016",
+        href: "portfolioModal1",
+        src: "img/portfolio/rps.png",
+        alt: "rps.png",
+        link: "http://joensg.github.io/Rock-Paper-Scissors/",
+        githublink: "https://github.com/Joensg/Rock-Paper-Scissors/",
+        description: "A single page application with RESTful server-side support built using the MEAN stack, where the user plays Rock-paper-scissors Game against the computer."
+    }, 
+    {
+        title: "Social Website Aggregator",
+        date: "March 2016",
+        href: "portfolioModal2",
+        src: "img/portfolio/siteace.png",
+        alt: "siteace.png",
+        link: "http://joensiteace.meteor.com/",
+        githublink: "https://github.com/Joensg/joensiteace.meteor.com/",
+        description: "A social website aggregator using MeteorJS."
+    },
+    {
+        title: "E-portfolio",
+        date: "November 2015",
+        href: "portfolioModal3",
+        src: "img/portfolio/eportfolio.png",
+        alt: "eportfolio.png",
+        link: "http://joensg.github.io/",
+        githublink: "https://github.com/Joensg/Joensg.github.io",
+        description: "My User Github Page Site. A site about me."
+    },
+    {
+        title: "Angular App1",
+        date: "February 2016",
+        href: "portfolioModal4",
+        src: "img/portfolio/angularapp1.png",
+        alt: "angularapp1.png",
+        link: "http://joensg.github.io/Web-Project-6-Angular-App1/",
+        githublink: "https://github.com/Joensg/Web-Project-6-Angular-App1/",
+        description: "Using the Angular Javascript framework, coded a one page application that downloads feed from a server and displays it in a table format."
+    },
+    {
+        title: "ConFusion",
+        date: "November 2015",
+        href: "portfolioModal5",
+        src: "img/portfolio/confusion.png",
+        alt: "confusion.png",
+        link: "http://joensg.github.io/Web-Project-5-ConFusion/",
+        githublink: "https://github.com/Joensg/Web-Project-5-ConFusion",
+        description: "An interactive website for a restaurant using bootstrap UI framework."
+    },
+    {
+        title: "Responsive Web page",
+        date: "November 2015",
+        href: "portfolioModal6",
+        src: "img/portfolio/responsivewebpage.png",
+        alt: "responsivewebpage.png",
+        link: "http://joensg.github.io/Web-Project-4-Responsive-web-page/",
+        githublink: "https://github.com/Joensg/Web-Project-4-Responsive-web-page",
+        description: "A responsive website about myself."
+    },
+    {
+        title: "Matching game",
+        date: "November 2015",
+        href: "portfolioModal7",
+        src: "img/portfolio/matchinggame.png",
+        alt: "matchinggame.png",
+        link: "http://joensg.github.io/Web-Project-3-Matching-game/",
+        githublink: "https://github.com/Joensg/Web-Project-3-Matching-game",
+        description: "A game to find and click on the extra smiley face present in the picture on the left side when compared to the picture on the right side."
+    },
+    {
+        title: "Simple Form",
+        date: "November 2015",
+        href: "portfolioModal8",
+        src: "img/portfolio/simpleform.png",
+        alt: "simpleform.png",
+        link: "http://joensg.github.io/Web-Project-1-Simple-form/",
+        githublink: "https://github.com/Joensg/Web-Project-1-Simple-form",
+        description: "A simple form for a website."
+    },
+    {
+        title: "Task List",
+        date: "February 2016",
+        href: "portfolioModal9",
+        src: "img/portfolio/tasklist.png",
+        alt: "tasklist.png",
+        link: "http://joensg.github.io/Web-Project-7-Task-List/index.html",
+        githublink: "https://github.com/Joensg/Web-Project-7-Task-List",
+        description: "A responsive website that presents a set of tasks to its users. The website is implemented using static content for the tasks, i.e. it is not data driven."
+    },
+    {
+        title: "Animals Data Driven Website",
+        date: "February 2016",
+        href: "portfolioModal10",
+        src: "img/portfolio/animalsddw.png",
+        alt: "animalsddw.png",
+        link: "http://joensg.github.io/Web-Project-8-Animals-Data-Driven-Website/",
+        githublink: "https://github.com/Joensg/Web-Project-8-Animals-Data-Driven-Website",
+        description: "A data driven website about animals using handlebarjs templates."
+    }],
+    certificateitem: [
+    {
+        number: "0",
+        src: "certificates/Coursera%20FXEYYPKMFY5N.png",
+        alt: "Coursera%20FXEYYPKMFY5N.png"
+    },
+    {
+        number: "1",
+        src: "certificates/Coursera%209KP9AA2Q4ABA.png",
+        alt: "Coursera%209KP9AA2Q4ABA.png"
+    },
+    {
+        number: "2",
+        src: "certificates/Coursera%20YG3N74UW4SVD.png",
+        alt: "Coursera%20YG3N74UW4SVD.png"
+    },
+    {
+        number: "3",
+        src: "certificates/Coursera%20LFATSDSLZHEP.png",
+        alt: "Coursera%20LFATSDSLZHEP.png"
+    },
+    {
+        number: "4",
+        src: "certificates/Coursera%20SVWBGWGE7MBD.png",
+        alt: "Coursera%20SVWBGWGE7MBD.png"
+    },
+    {
+        number: "5",
+        src: "certificates/Coursera%20N228ZZPXP57H.png",
+        alt: "Coursera%20N228ZZPXP57H.png"
+    },
+    {
+        number: "6",
+        src: "certificates/Coursera%20androidpart1.png",
+        alt: "Coursera%20androidpart1.png"
+    },
+    {
+        number: "7",
+        src: "certificates/Coursera%20androidpart2.png",
+        alt: "Coursera%20androidpart2.png"
+    },
+    {
+        number: "8",
+        src: "certificates/Coursera%20datascitoolbox.png",
+        alt: "Coursera%20datascitoolbox.png"
+    },
+    {
+        number: "9",
+        src: "certificates/Coursera%20GZCQ5F4MMQWB.png",
+        alt: "Coursera%20GZCQ5F4MMQWB.png"
+    },
+    {
+        number: "10",
+        src: "certificates/Coursera%20interactivepython1.png",
+        alt: "Coursera%20interactivepython1.png"
+    },
+    {
+        number: "11",
+        src: "certificates/Coursera%20interactivepython2.png",
+        alt: "Coursera%20interactivepython2.png"
+    },
+    {
+        number: "12",
+        src: "certificates/Coursera%20J2GMJGB2FABA.png",
+        alt: "Coursera%20J2GMJGB2FABA.png"
+    },
+    {
+        number: "13",
+        src: "certificates/Coursera%20py1%209DZSHBNBJGCQ.png",
+        alt: "Coursera%20py1%209DZSHBNBJGCQ.png"
+    },
+    {
+        number: "14",
+        src: "certificates/Coursera%20py2%207KVEDGH5TWYU.png",
+        alt: "Coursera%20py2%207KVEDGH5TWYU.png"
+    },
+    {
+        number: "15",
+        src: "certificates/Coursera%20YBGFQW5NTEXN.png",
+        alt: "Coursera%20YBGFQW5NTEXN.png"
+    },
+    {
+        number: "16",
+        src: "certificates/Coursera%20CGRE3UMZTU2U.png",
+        alt: "Coursera%20CGRE3UMZTU2U.png"
+    }]
 };
